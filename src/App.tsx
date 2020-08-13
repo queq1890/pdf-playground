e3ec54997cec5df766dce11e4612e7e8a70cfa32
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-08-13 16:50:47 +0700

    pdf to image example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Pdf2Image from "./Pdf2Image";
 
 function App() {
+  const [img, setImg] = useState("");
+
+  const convertPDFToBase64Image = async (e: any) => {
+    try {
+      const file = e.target.files[0];
+      const url = URL.createObjectURL(file);
+      const pdf2image = await Pdf2Image.open(url);
+
+      const images = await pdf2image.getAllImageDataUrl({
+        width: 400,
+        height: 400,
+      });
+
+      setImg(images[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,14 @@ function App() {
         >
           Learn React
         </a>
+        <img src={img} alt="fetched" />
+
+        <input
+          type="file"
+          name="upload"
+          accept="application/pdf,application/vnd.ms-excel"
+          onChange={convertPDFToBase64Image}
+        />
       </header>
     </div>
   );
